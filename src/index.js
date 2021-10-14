50274cb55746356a10e5345f7015dfbd3c7cc120
akimediz <79215185+akimediz@users.noreply.github.com>
2021-10-13 19:49:24 -0700

    changes made

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const fname = "Abdulakeem";
+const lname = "Morakinyo";
+const lucky_num = 8;
+
+ReactDOM.render(
+  <div>
+    <h2>Hello {fname + " " + lname}</h2>
+    <p>your lucky num is {4 + 7}</p>
+  </div>,
+  document.getElementById("root")
+);
